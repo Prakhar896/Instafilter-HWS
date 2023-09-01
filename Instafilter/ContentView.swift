cc7fedc9a345e78b9a9825509e87440f665f259f
Prakhar Trivedi <Prakhar896@users.noreply.github.com>
2023-09-01 12:32:58 +0800

    reset content view to actually work on instafilter

--- Instafilter/ContentView.swift
+++ Instafilter/ContentView.swift
@@ -8,40 +8,9 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State var image: Image?
-    @State var inputImage: UIImage?
-    @State var showingPhotoPicker = false
-    
     var body: some View {
-        VStack(spacing: 10) {
-            image?
-                .resizable()
-                .scaledToFit()
-                .cornerRadius(10)
-                .padding()
-            
-            Button("Pick Photo") {
-                showingPhotoPicker = true
-            }
+        Text("Hello, World!")
             .padding()
-            
-            Button("Save Photo") {
-                guard let inputImage = inputImage else { return }
-                ImageSaver().writeToPhotoAlbum(image: inputImage)
-            }
-            .padding()
-        }
-        .sheet(isPresented: $showingPhotoPicker) {
-            ImagePicker(image: $inputImage)
-        }
-        .onChange(of: inputImage) { _ in
-            loadImage()
-        }
-    }
-    
-    func loadImage() {
-        guard let inputImage = inputImage else { return }
-        image = Image(uiImage: inputImage)
     }
 }
 
