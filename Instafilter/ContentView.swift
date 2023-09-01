859382883828372128f9e6aa407519426ed477fc
Prakhar Trivedi <Prakhar896@users.noreply.github.com>
2023-09-01 15:08:37 +0800

    app complete

--- Instafilter/ContentView.swift
+++ Instafilter/ContentView.swift
@@ -12,9 +12,12 @@ import CoreImage.CIFilterBuiltins
 struct ContentView: View {
     @State private var image: Image?
     @State private var inputImage: UIImage?
+    @State private var processedImage: UIImage?
+    @State private var saveStatus = false
     
     @State private var showingPickerSheet = false
     @State private var showingFilterSheet = false
+    @State private var showingSaveStatusAlert = false
     
     @State private var filterIntensity = 0.5
     @State private var currentFilter: CIFilter = CIFilter.sepiaTone()
@@ -78,9 +81,13 @@ struct ContentView: View {
                 
                 Button("Cancel", role: .cancel) {}
             } message: {
-                Text("")
+                Text("Select a filter")
             }
-
+            .alert(saveStatus ? "Filtered image saved!": "Error in saving image", isPresented: $showingSaveStatusAlert) {
+                Button(saveStatus ? "Great!": "OK") {}
+            } message: {
+                Text(saveStatus ? "Your new processed photo has been saved to your photo library!": "An error occurred in saving your filtered photo to your library. This might be due to insufficient permissions granted to the app. Please try again!")
+            }
         }
     }
     
@@ -109,6 +116,7 @@ struct ContentView: View {
         
         if let cgImg = context.createCGImage(outputImage, from: outputImage.extent) {
             let uiImage = UIImage(cgImage: cgImg)
+            processedImage = uiImage
             image = Image(uiImage: uiImage)
         }
     }
@@ -119,7 +127,20 @@ struct ContentView: View {
     }
     
     func save() {
+        guard let processedImage = processedImage else { return }
         
+        let imageSaver = ImageSaver()
+        imageSaver.successHandler = {
+            print("Save successful!")
+            saveStatus = true
+            showingSaveStatusAlert = true
+        }
+        imageSaver.errorHandler = {
+            print("Error in save: \($0.localizedDescription)")
+            saveStatus = false
+            showingSaveStatusAlert = true
+        }
+        imageSaver.writeToPhotoAlbum(image: processedImage)
     }
 }
 
